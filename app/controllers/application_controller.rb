3afdc72155a7a8d9132cb9add25c442048941a2a
minarai847 <72494683+minarai847@users.noreply.github.com>
2020-10-12 20:18:36 +0900

    ユーザー認証機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  #ログインしていないユーザーをログインページの画面に促すことができます
+  before_action :authenticate_user!
+#悪意のあるユーザーの入力に対してセキュリティ対策を行わないと保存できない仕組みがあります。
+#この記述をすることで、全てのアクションをするにあたって、名前を聞いてくる
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  private
+  def configure_permitted_parameters
+    #「nameカラム」を追加したので、このメソッドを使用し、「name」キーの内容の保存をpermitメソッドで許可しています。
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+end
 end
