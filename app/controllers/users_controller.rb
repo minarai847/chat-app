9f53143fab8a4f809939c3b554d756625a2f7804
minarai847 <72494683+minarai847@users.noreply.github.com>
2020-10-12 20:43:53 +0900

    ユーザー編集機能の実装

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,29 @@
+class UsersController < ApplicationController
+  #編集
+  def edit 
+  end
+
+  #更新　
+  def update
+    #ログインしているユーザーの情報を更新する
+    #ここで分岐させている
+    #更新が保存された場合、チャット画面に戻る
+    if current_user.update(user_params)
+      redirect_to root_path
+      #更新が保存されなかった場合、そのままedit画面のまま
+    else
+      render :edit
+    end
+
+  end
+
+
+
+  private
+
+  def user_params
+    #ユーザーの名前とemailの情報を編集を許可している
+    params.require(:user).permit(:name, :email)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'messages/index'
+  #get 'messages/index'
    root to: "messages#index"
+   #ユーザー編集に必要なルーティングは、editとupdate
+   resources :users, only: [:edit, :update]
 end
