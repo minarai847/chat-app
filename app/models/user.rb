3c302f640ce7ea44db9a24919b390a7326e33055
minarai847 <72494683+minarai847@users.noreply.github.com>
2020-10-12 21:33:35 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         #validates カラム,presence:true
+         #nameカラムが空の場合データベースに保存しない
+         validates :name, presence:true
+
 end
