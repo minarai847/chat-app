988c7de6be7234b34e12c5ab9fd3b1c6889c6443
minarai847 <72494683+minarai847@users.noreply.github.com>
2020-10-13 11:38:02 +0900

    チャットルームの作成機能の実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -4,5 +4,24 @@ class RoomsController < ApplicationController
     #値が空のRoomインスタンスを@roomに代入している
     @room = Room.new
   end
+
+  def create
+    @room = Room.new(room_params)
+    #保存(save)が成功したかどうかで処理を分岐させている
+    #成功した場合、ルートパスにリダイレクトし
+    #失敗した場合、rooms/new.html.erbのページを表示して
+    if @room.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  
+  def room_params
+    params.require(:room).permit(:name, user_ids:[])
+  end
+
   
 end
