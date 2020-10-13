3a47faaa05e809268c7b89b5d390cea0a846062f
minarai847 <72494683+minarai847@users.noreply.github.com>
2020-10-13 19:19:29 +0900

    Active Storageの導入

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,6 +3,9 @@ class Message < ApplicationRecord
   #１人のユーザーは、複数のメッセージを送信できる
   belongs_to :room
   belongs_to :user
+  #各レコードとファイルを１対１の関係で紐づけるメソッド
+  #imageファイル
+  has_one_attached :image
   #contentカラム（メッセージが入るところ）が空だとDBには保存しないという指令
   validates :content, presence: true
 end
