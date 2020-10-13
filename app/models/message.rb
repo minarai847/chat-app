48a07773af2cf31f251d42ed90dda6dbd06c1be7
minarai847 <72494683+minarai847@users.noreply.github.com>
2020-10-13 18:02:37 +0900

    空のメッセージが送信できないバリデーションの設定

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,4 +3,6 @@ class Message < ApplicationRecord
   #１人のユーザーは、複数のメッセージを送信できる
   belongs_to :room
   belongs_to :user
+  #contentカラム（メッセージが入るところ）が空だとDBには保存しないという指令
+  validates :content, presence: true
 end
